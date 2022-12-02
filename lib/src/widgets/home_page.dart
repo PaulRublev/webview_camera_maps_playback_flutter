13b11437bb254e4a36c00f739e6495f3ca2fb1df
paul rublev <paulrublev@Mac.local>
2022-12-02 22:33:53 +0500

    Add camera

--- lib/src/widgets/home_page.dart
+++ lib/src/widgets/home_page.dart
@@ -1,48 +1,120 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
+  late TabController _tabController;
+  CameraController? _cameraController;
+  late List<CameraDescription> cameras;
+  final List<XFile> _pictures = [];
+  late Future<void> initCam;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
     });
+    initCam = initCamera();
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (!_cameraController!.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      _cameraController!.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      await initCamera();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+            _tabController.index == 1 ? 'Images gallery' : 'Camera preview'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          FutureBuilder(
+            future: initCam,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return _cameraController!.value.isInitialized
+                    ? CameraPreview(_cameraController!)
+                    : const SizedBox();
+              }
+              return const SizedBox();
+            },
+          ),
+          ListView.builder(
+            itemCount: _pictures.length,
+            itemBuilder: ((context, index) {
+              return Image.file(
+                File(_pictures[index].path),
+                fit: BoxFit.fitWidth,
+              );
+            }),
+          ),
+        ],
+      ),
+      floatingActionButton: _tabController.index == 1
+          ? null
+          : FloatingActionButton(
+              onPressed: () async {
+                _pictures.add(await _cameraController!.takePicture());
+                setState(() {});
+              },
+              tooltip: 'Snapshot',
+              child: const Icon(Icons.camera),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _tabController.index,
+          onTap: (value) {
+            setState(() {
+              _tabController.index = value;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: 'Camera',
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.image),
+              label: 'Gallery',
+            ),
+          ]),
     );
+  }
+
+  @override
+  void dispose() {
+    _cameraController!.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Future<void> initCamera() async {
+    cameras = await availableCameras();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.max);
+    await _cameraController!.initialize();
+    setState(() {});
   }
 }
