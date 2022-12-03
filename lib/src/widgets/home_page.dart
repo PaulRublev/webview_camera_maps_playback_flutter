389e63671f67f3c92a9bb0b52ccb6667149f5823
paul rublev <paulrublev@Mac.local>
2022-12-03 16:14:43 +0500

    Add skeleton project

--- lib/src/widgets/home_page.dart
+++ lib/src/widgets/home_page.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-
-import 'video_player_buttons.dart';
-import 'video_player_navigation_panel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,58 +8,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late VideoPlayerController _controller;
-  bool _isControlVisible = true;
-  final String _url =
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
-
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(_url)
-      ..initialize().then((value) {
-        setState(() {});
-      });
-    _controller
-      ..addListener(() {
-        setState(() {});
-      })
-      ..play();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_url.split(r'/').last),
+        title: Text(''),
         centerTitle: true,
       ),
       body: SafeArea(
         child: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isControlVisible = !_isControlVisible;
-                      });
-                    },
-                    child: Stack(
-                      children: [
-                        VideoPlayer(_controller),
-                        _isControlVisible
-                            ? VideoPlayerButtons(controller: _controller)
-                            : const SizedBox(),
-                        _isControlVisible
-                            ? VideoPlayerNavigationPanel(
-                                controller: _controller)
-                            : const SizedBox(),
-                      ],
-                    ),
-                  ),
-                )
-              : Container(),
+          child: Container(),
         ),
       ),
     );
@@ -71,7 +30,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 }
